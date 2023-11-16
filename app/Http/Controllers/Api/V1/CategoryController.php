17eb7455eb8231ca403d0e2a62cb50992ace8e5f
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-11-16 21:41:14 +0545

    endpoint to store category

---
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:191|unique:categories,name',
+            'rank' => 'required|integer|min:1',
+        ]);
+
+        $category = Category::create($validated);
+
+        return response()->json($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('Api/V1')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
+
+    Route::post('categories', [CategoryController::class, 'store']);
 });
