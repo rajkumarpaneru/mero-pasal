a5deca2d4037b5e447410ec8fbde147fa94861d5
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-02 21:28:46 +0545

    add resource routes for sub-category

--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\SubCategoryController;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\CategoryController;
 use Illuminate\Support\Facades\Route;
@@ -7,9 +8,21 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Api/V1')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 
+    //categories
     Route::get('categories', [CategoryController::class, 'index']);
     Route::post('categories', [CategoryController::class, 'store']);
     Route::get('categories/{category}', [CategoryController::class, 'show']);
     Route::post('categories/{category}', [CategoryController::class, 'update']);
     Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+    //sub-categories
+    Route::get('categories/{category}/sub-categories', [SubCategoryController::class, 'index']);
+    Route::post('categories/{category}/sub-categories',
+        [SubCategoryController::class, 'store']);
+    Route::get('categories/{category}/sub-categories/{subCategory}',
+        [SubCategoryController::class, 'show']);
+    Route::post('categories/{category}/sub-categories/{subCategory}',
+        [SubCategoryController::class, 'update']);
+    Route::delete('categories/{category}/sub-categories/{subCategory}',
+        [SubCategoryController::class, 'destroy']);
 });
