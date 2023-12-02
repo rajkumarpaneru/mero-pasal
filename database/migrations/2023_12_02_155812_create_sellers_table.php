14a4e57d597fbcb8c3fcd720d79d756b15bd48ff
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-02 21:48:34 +0545

    model and migration for seller

---
+++ database/migrations/2023_12_02_155812_create_sellers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users');
+            $table->string('name')->index();
+            $table->string('address')->nullable();
+            $table->string('registration_type')->nullable();
+            $table->string('registration_number')->nullable();
+            $table->timestamp('verified_at')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+};
