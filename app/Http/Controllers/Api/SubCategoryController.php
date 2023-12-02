9737cae65419c950de0152807ba957fc1783506c
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-02 21:40:20 +0545

    remove unused import

--- app/Http/Controllers/Api/SubCategoryController.php
+++ app/Http/Controllers/Api/SubCategoryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use App\Models\SubCategory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
