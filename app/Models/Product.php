f9580a95bae94b96eeaeb93bab5fbe16a7b73cce
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2024-02-24 22:15:36 +0545

    product's relations definition

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,19 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function subCategory()
+    {
+        return $this->belongsTo(SubCategory::class, 'sub_category_id', 'id');
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class, 'brand_id', 'id');
+    }
+
+    public function productFeatures()
+    {
+        return $this->hasMany(ProductFeature::class, 'product_id', 'id');
+    }
 }
