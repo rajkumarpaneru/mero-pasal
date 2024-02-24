97ceec35f720f0a96271efe6e82761650ffe156e
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2024-02-24 22:11:40 +0545

    model and migration for product-features

---
+++ database/migrations/2024_02_24_162049_create_product_features_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_features', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('product_id');
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->text('feature');
+            $table->unsignedSmallInteger('rank');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_features');
+    }
+};
